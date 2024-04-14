89c93c6479e48c0ae9b42e19e9c0ea83781dbe44
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-04-14 22:16:31 +0100

    Create const var for navigation list labels

--- lib/utills/consts/text.dart
+++ lib/utills/consts/text.dart
@@ -1,4 +1,10 @@
 class AText {
+  // navigation
+  static const String home = "Home";
+  static const String campaigns = "Campaigns";
+  static const String wallet = "Wallet";
+  static const String more = "More";
+
   static const String signup = "Sign Up";
   static const String signin = "Sign In";
   static const String signout = "Sign Out";
--- lib/widgets/navigation/navigation_list.dart
+++ lib/widgets/navigation/navigation_list.dart
@@ -1,31 +1,32 @@
 import 'package:enfluwence/utills/consts/icons.dart';
+import 'package:enfluwence/utills/consts/text.dart';
 import 'package:flutter/material.dart';
 
 class ANavigationList {
   static List<dynamic> navigationList = [
     {
-      "label": "Home",
+      "label": AText.home,
       "icon": AIcons.home,
       "screen": const Center(
         child: Text("Home page"),
       )
     },
     {
-      "label": "Campaigns",
+      "label": AText.campaigns,
       "icon": AIcons.campaigns,
       "screen": const Center(
         child: Text("Campaigns page"),
       )
     },
     {
-      "label": "Wallet",
+      "label": AText.wallet,
       "icon": AIcons.wallet,
       "screen": const Center(
         child: Text("Wallet page"),
       )
     },
     {
-      "label": "More",
+      "label": AText.more,
       "icon": AIcons.widget,
       "screen": const Center(
         child: Text("More page"),
