204f1264e360b471d16592147529c3e3ea3beea8
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-04-15 18:06:09 +0100

    Create folder for home screen

--- lib/widgets/navigation/navigation_list.dart
+++ lib/widgets/navigation/navigation_list.dart
@@ -1,4 +1,4 @@
-import 'package:enfluwence/pages/influencers/screens/home.dart';
+import 'package:enfluwence/pages/influencers/screens/home/home.dart';
 import 'package:enfluwence/utills/consts/icons.dart';
 import 'package:enfluwence/utills/consts/text.dart';
 import 'package:flutter/material.dart';
