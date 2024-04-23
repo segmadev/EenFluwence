ebc223ae8a44795269af8aaf283e2abd1bfeaf6b
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-04-23 20:59:31 +0100

    create mockup text for pages in moreNavigationList

--- lib/widgets/navigation/navigation_list.dart
+++ lib/widgets/navigation/navigation_list.dart
@@ -1,3 +1,4 @@
+import 'package:enfluwence/pages/auth/screens/auth.dart';
 import 'package:enfluwence/pages/influencers/screens/profile/profile.dart';
 import 'package:enfluwence/pages/influencers/screens/tasks/campaign/campaign_page.dart';
 import 'package:enfluwence/pages/influencers/screens/home/home.dart';
@@ -5,6 +6,7 @@ import 'package:enfluwence/pages/influencers/screens/wallet/wallet_page.dart';
 import 'package:enfluwence/utills/consts/colors.dart';
 import 'package:enfluwence/utills/consts/icons.dart';
 import 'package:enfluwence/utills/consts/text.dart';
+import 'package:enfluwence/widgets/navigation/back_navigation.dart';
 import 'package:enfluwence/widgets/navigation/more_navigation_menu.dart';
 import 'package:flutter/material.dart';
 
@@ -33,41 +35,33 @@ class ANavigationList {
     },
     {
       "label": AText.settings,
-      "screen": const Center(
-        child: CampaignScreen(),
+      "screen": const BackNavigation(
+        body: Center(child: Text("Settings Page")),
       )
     },
     {
       "label": AText.support,
-      "screen": const Center(
-        child: WalletScreen(),
+      "screen": const BackNavigation(
+        body: Center(child: Text("Support Page")),
       )
     },
     {
       "label": AText.legal,
-      "screen": const Center(
-        child: Text("More page"),
+      "screen": const BackNavigation(
+        body: Center(child: Text("Legal Page")),
       )
     },
     {
       "label": AText.about,
-      "screen": const Center(
-        child: Text("More page"),
-      )
-    },
-    {
-      "label": AText.legal,
-      "screen": const Center(
-        child: Text("More page"),
+      "screen": const BackNavigation(
+        body: Center(child: Text("About page")),
       )
     },
     {
       "label": AText.logOut,
       "color": AColor.danger.withOpacity(0.4),
       "icon": AIcons.logout,
-      "screen": const Center(
-        child: Text("More page"),
-      )
+      "screen": AuthScreen(),
     },
   ];
 }
